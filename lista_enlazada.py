f2604398ec5ebdf6003f7fe0ddad25c3064fcc2f
oswaldosanchez7277-stack <oswaldo.sanchez7277@alumnos.udg.mx>
2025-10-09 11:56:33 -0600

    Prueba

---
+++ lista_enlazada.py
@@ -0,0 +1,26 @@
+class nodoDoble:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None #apunta al siguiente nodo
+        self.anterior = None  #apunta al nodo anterior
+
+nodo1 = nodoDoble(10)
+nodo2 = nodoDoble(20)
+nodo3 = nodoDoble(30)
+nodo1.siguiente = nodo2 
+nodo2.anterior = nodo1  
+nodo2.siguiente = nodo3 
+nodo3.anterior = nodo2  
+cabeza = nodo1
+current = cabeza
+while current:
+    print(current.dato, end=" <-> ")
+    current=current.siguiente
+print("None")
+current = cabeza
+while current.siguiente:
+    current=current.siguiente
+while current:
+    print(current.dato, end=" <-> ")
+    current=current.anterior
+print("None")
